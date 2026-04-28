c78b00e2df0e5b50c882745d8769c373c12402ff
“satyajitus14” <sjbarua14@gmail.com>
2026-04-28 18:36:38 -0400

    all photos page done

---
+++ src/app/all-photos/page.jsx
@@ -0,0 +1,23 @@
+import PhotoCards from '@/components/PhotoCards';
+import React from 'react';
+
+const AllPhotosPage = async() => {
+     const res = await fetch('https://project-on-pix-gen-image-generator.vercel.app/lib/data.json')
+    const photos = await res.json()
+    console.log(photos);
+
+    return (
+        <div>
+            <h1 className='text-2xl font-bold m-4'>All Photos</h1>
+            <div className='grid grid-cols-4 gap-5'>
+                {
+                    photos.map(photo => <PhotoCards key={photo.id} photo={photo}>
+                        
+                    </PhotoCards>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default AllPhotosPage;
---
+++ src/app/pricing/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PricingPage = () => {
+    return (
+        <div>
+            This is Pricing Page
+        </div>
+    );
+};
+
+export default PricingPage;
